ce02f8b322e7a9446a9e44681e332bc0c5497358
ASSabbir <atiksabbir1125@gmail.com>
2025-12-15 02:32:40 +0600

    added the section

--- src/Root.jsx
+++ src/Root.jsx
@@ -5,6 +5,7 @@ import logovideo from '../src/assets/video/logo2.mp4'
 import { useRef } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import Spline from '@splinetool/react-spline';
 
 gsap.registerPlugin(useGSAP); 
 
@@ -45,6 +46,9 @@ const Root = () => {
                 playsInline
                 id='loading_video'
                 className="w-full h-auto absolute top-0"></video>
+
+        <Spline scene="https://prod.spline.design/JgOocE1eYq6WwlpM/scene.splinecode" />
+
         </div>
     );
 };
