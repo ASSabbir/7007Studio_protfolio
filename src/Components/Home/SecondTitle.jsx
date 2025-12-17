04cc4dce6f974c46f127cca5a6efea69182c4767
ASSabbir <atiksabbir1125@gmail.com>
2025-12-18 03:36:00 +0600

    added

--- src/Components/Home/SecondTitle.jsx
+++ src/Components/Home/SecondTitle.jsx
@@ -1,13 +1,26 @@
+import { div } from 'framer-motion/client';
 import React from 'react';
-
+import { IoMdArrowUp } from "react-icons/io";
 const SecondTitle = () => {
     return (
-        <div className=" text-9xl  px-20 leading-none py-[10vw] normal-case w-full bg-black  font-font2">
-                        <h1 className="relative leading-28 ">
-                            High-End 3D Visuals. Built for Impact.
-                        </h1>
-                        <h1 className='text-3xl  font-extralight font-dmsans mt-10'>We turn complex ideas into effortless experiences</h1>
-                    </div>
+        <div className='bg-black'>
+            <div className=" text-9xl  px-20 leading-none pt-[10vw] normal-case w-full bg-black  font-font2">
+                <h1 className="relative leading-28 ">
+                    High-End 3D Visuals. Built for Impact.
+                </h1>
+                <h1 className='text-3xl  font-extralight font-dmsans mt-10'>We turn complex ideas into effortless experiences</h1>
+
+            </div>
+            <div className=' flex justify-end px-[10vw] pb-[10vw]'>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/JFNUVJtlQDU?si=zLHjhD3NwWulf5ig" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+            <div className="flex group  mt-36  gap-2">
+                            <button className="uppercase text-xl  group-hover:text-white group-hover:bg-zinc-900 border-[1px] px-4 py-2 rounded-full duration-500 border-zinc-900 tracking-tighter">Investor deck</button>
+                            <div className="w-12 h-12 group-hover:bg-zinc-900 border-[1px] border-zinc-900 rounded-full flex duration-500 justify-center items-center text-2xl ">
+                                <IoMdArrowUp className="rotate-45 duration-500 group-hover:rotate-[405deg] group-hover:text-white "></IoMdArrowUp>
+                            </div>
+                        </div>
+        </div>
     );
 };
 
--- src/Root.jsx
+++ src/Root.jsx
@@ -6,36 +6,37 @@ import { useRef } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import Footer from './Components/Footer';
+import { ReactLenis, useLenis } from 'lenis/react'
+import Lenis from 'lenis';
 
-
-gsap.registerPlugin(useGSAP); 
+gsap.registerPlugin(useGSAP);
 
 const Root = () => {
 
-    useGSAP(()=>{
-        const tl =gsap.timeline()
-        tl.to('#loading_video',{
-            
-            duration:1.4
+    useGSAP(() => {
+        const tl = gsap.timeline()
+        tl.to('#loading_video', {
+
+            duration: 1.4
         })
-        tl.to('#loading_video',{
-            opacity:0,
-            duration:0.35
+        tl.to('#loading_video', {
+            opacity: 0,
+            duration: 0.35
         })
-        tl.to('#loading_video',{
-            display:'none'
+        tl.to('#loading_video', {
+            display: 'none'
         })
-        tl.from('#title-logo',{
-            y:50,
-            opacity:0
+        tl.from('#title-logo', {
+            y: 50,
+            opacity: 0
         })
-        tl.to('#title-logo',{
-            y:0,
-            opacity:1,
-            duration:0.5,
+        tl.to('#title-logo', {
+            y: 0,
+            opacity: 1,
+            duration: 0.5,
             ease: "power4.out",
         })
-        
+
     })
     return (
         <div className=' relative h-screen text-white'>
@@ -47,8 +48,22 @@ const Root = () => {
                 playsInline
                 id='loading_video'
                 className="w-full h-auto absolute top-0"></video>
-<Footer></Footer>
-        
+            <Footer></Footer>
+            {/* <ReactLenis
+                root
+                options={{
+                    duration: 1.2,        // scroll duration in seconds (default: 1.2)
+                    easing: (t) => 1 - Math.pow(1 - t, 3), // custom easing curve
+                   
+                    gestureDirection: 'both', // allow both mouse wheel + touchpad gestures
+                    smoothWheel: true,     // smooth scroll for mouse wheel
+                    smoothTouch: true,    // smooth scroll for touch devices
+                    touchMultiplier: 1.5,  // multiplier for touch scroll speed
+                    wheelMultiplier: 1.5,    // multiplier for wheel scroll speed
+                    infinite: false,       // loop scrolling
+                }}
+            /> */}
+
 
         </div>
     );
