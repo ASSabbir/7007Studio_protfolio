84db223c0aea7d88b245ee09446f424a1ac4910b
mustazir <billah15-5193@diu.edu.bd>
2025-12-18 01:55:29 +0600

    contact

---
+++ src/Pages/Contact/Contct.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Spline from "@splinetool/react-spline";
+
+const Contact = () => {
+  return (
+    <div>
+      <Spline scene="https://prod.spline.design/0Swr3dhMkTmmzhgH/scene.splinecode" />
+    </div>
+  );
+};
+
+export default Contact;
