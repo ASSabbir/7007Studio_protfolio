fdf204424c8a172831bdc5c982209e318b03b719
Abdullah Al Noman <130217084+AbdullahAlNoman20@users.noreply.github.com>
2026-01-10 20:34:31 +0600

    Delete and Optimize

--- src/Pages/Service/Services.jsx
+++ src/Pages/Service/Services.jsx
@@ -1,25 +1,12 @@
 import React, { useState, useRef } from "react";
 import HeroSection from "./HeroSection";
-import ServicesContent from "./ServicesContent";
-import MeetingSection from "./MeetingSection"; // Import
-import { gigServices, customServices } from "./servicesData";
-import "./styles";
 import ServiceMotivation from "./serviceMotivation";
 import Package from "./Package";
 import Calender from "./Calender";
 import Description from "./Description";
-import { motion } from 'framer-motion';
 
 const Services = () => {
-  const [activeTab, setActiveTab] = useState("gig");
-  const [selectedService, setSelectedService] = useState(null);
   const containerRef = useRef(null);
-  const detailsRef = useRef(null);
-
-  const handleTabChange = (tab) => {
-    setActiveTab(tab);
-    setSelectedService(null);
-  };
 
   return (
     <div className="relative min-h-screen text-white font-KronaOne" ref={containerRef}>
@@ -50,25 +37,25 @@ const Services = () => {
 
       <section className="w-full bg-black text-white font-kronaOne px-6 md:px-12 lg:px-48">
 
-  <p className="w-full text-4xl md:text-6xl lg:text-7xl font-kronaOne">
-    The Internal <span className="text-red-500 text-gray-300 text-lg md:text-xl font-kronaOne mt-6 w-full">
-      Tech Team for you
-    </span> 
-  </p>
+        <p className="w-full text-4xl md:text-6xl lg:text-7xl font-kronaOne">
+          The Internal <span className="text-red-500 text-gray-300 text-lg md:text-xl font-kronaOne mt-6 w-full">
+            Tech Team for you
+          </span>
+        </p>
 
-  <p className="text-gray-300 text-lg md:text-xl font-kronaOne leading-relaxed w-full"><br />
-    We build the custom tools and automations that power
-    <span className="text-red-500"> TVC, film, and game agencies</span>.
-    We act as your internal technical partner, providing the infrastructure
-    you need to scale production—from quick, ready-to-go gigs to
-    bespoke internal systems.
-  </p>
+        <p className="text-gray-300 text-lg md:text-xl font-kronaOne leading-relaxed w-full"><br />
+          We build the custom tools and automations that power
+          <span className="text-red-500"> TVC, film, and game agencies</span>.
+          We act as your internal technical partner, providing the infrastructure
+          you need to scale production—from quick, ready-to-go gigs to
+          bespoke internal systems.
+        </p>
 
-  <p className="mt-8 text-red-500 text-lg md:text-xl font-kronaOne tracking-wide">
-    [ Book a 30-min Session ]
-  </p>
+        <p className="mt-8 text-red-500 text-lg md:text-xl font-kronaOne tracking-wide">
+          [ Book a 30-min Session ]
+        </p>
 
-</section>
+      </section>
 
 
       {/* Meeting Section*/}
--- src/Pages/Service/serviceMotivation.jsx
+++ src/Pages/Service/serviceMotivation.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion} from 'framer-motion';
 
 const ServiceMotivation = () => {
   const [isHovered, setIsHovered] = useState(false);
